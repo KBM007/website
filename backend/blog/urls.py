341b0d9208f578191147bfa55b6c2632f69f7f72
KBM007 <maityk007@gmail.com>
2023-02-27 18:16:02 +0530

    day 3

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import urls as users_urls
+from posts import urls as posts_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include("rest_framework.urls", namespace="rest_framework")),
+    path("api/posts/", include(posts_urls)),
     path('api/users/', include(users_urls)),
 ]
